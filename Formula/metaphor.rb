6c760974f3d216783ad4d661336791fed5522eea
Shinya Oguri <ogrsny@gmail.com>
2026-05-08 13:16:16 +0900

    Add metaphor formula

---
+++ Formula/metaphor.rb
@@ -0,0 +1,23 @@
+class Metaphor < Formula
+  desc "Command-line tools for the metaphor Swift + Metal creative coding library"
+  homepage "https://github.com/shinyaoguri/metaphor-cli"
+  url "https://github.com/shinyaoguri/metaphor-cli/releases/download/v0.1.0/metaphor-cli_v0.1.0_source.tar.gz"
+  version "0.1.0"
+  sha256 "c6d1fc7617479ed9bc237c2da6fb11360151bf8d998f2367043ade76565963d0"
+  license "MIT"
+  head "https://github.com/shinyaoguri/metaphor-cli.git", branch: "main"
+
+  depends_on xcode: ["15.0", :build]
+  depends_on macos: :sonoma
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/metaphor"
+    (share/"metaphor").install "Templates" => "templates"
+  end
+
+  test do
+    assert_match "metaphor", shell_output("#{bin}/metaphor version")
+    assert_match "Templates:", shell_output("#{bin}/metaphor --help")
+  end
+end
